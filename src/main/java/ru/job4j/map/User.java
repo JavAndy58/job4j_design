1f558450dfe2e2e4b0c5059c096a28257e1e06fa
JavAndy58 <digital.designs.russia@gmail.com>
2021-09-29 18:19:55 +0300

    1. Создать модель User [#999]

---
+++ src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,13 @@
+package ru.job4j.map;
+
+public class User {
+    private String name;
+    private String children;
+    private String birthday;
+
+    public User(String name, String children, String birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
