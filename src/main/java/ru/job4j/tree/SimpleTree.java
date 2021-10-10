ba4b653565d0d60b3fd5a38066ad63d9b50839f2
JavAndy58 <digital.designs.russia@gmail.com>
2021-10-10 13:37:49 +0300

    1. Создать элементарную структуру дерева [#1711 #213370]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,8 +1,8 @@
 package ru.job4j.tree;
 
-import java.util.*;
-import java.util.function.Predicate;
-import static com.puppycrawl.tools.checkstyle.grammar.javadoc.JavadocLexer.value;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
