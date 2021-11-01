923c543129aeaaaebccecf294e18f2463a261fa6
JavAndy58 <digital.designs.russia@gmail.com>
2021-11-01 20:29:33 +0300

    1. Читаем файл конфигурации [#858]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -4,22 +4,17 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class EvenNumberFile {
-
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("even.txt")) {
+        try(FileInputStream in = new FileInputStream("even.txt")) {
             StringBuilder text = new StringBuilder();
             int read;
-            int number;
             while ((read = in.read()) != -1) {
-                text.append((char) read);
-            }
-            String[] lines = text.toString().split(System.lineSeparator());
-            for (String line : lines) {
-                number = Integer.parseInt(line);
-                System.out.println(number + " " + (number % 2 == 0));
+                System.out.println(text.append((int) read));
+
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
