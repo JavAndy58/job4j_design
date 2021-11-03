31e388a244407c7507897c235ef52c080acab8f3
JavAndy58 <digital.designs.russia@gmail.com>
2021-11-03 11:05:11 +0300

    1

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class LogFilter {
@@ -12,12 +11,11 @@ public class LogFilter {
         List<String> list = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             String line;
-            String[] lines;
-            String search = "404";
+            String search = " 404 ";
+
+
             while ((line = in.readLine()) != null) {
-                lines = line.split(" ");
-                boolean found = Arrays.asList(lines).contains(search);
-                if (found) {
+                if (line.contains(search)) {
                     list.add(line);
                     list.add(System.lineSeparator());
                 }
