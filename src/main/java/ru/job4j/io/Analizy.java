9ab4afe40bca4d0723277b6bfdebeda6041cfe75
JavAndy58 <digital.designs.russia@gmail.com>
2021-11-04 13:00:08 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -5,30 +5,26 @@ import java.util.Objects;
 
 public class Analizy {
     public void unavailable(String source, String target) {
+        String line;
+        int rsl = 0;
+
         try (BufferedReader in = new BufferedReader(new FileReader(source));
         PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            String line;
-            int rsl = 0;
-
             while ((line = in.readLine()) != null) {
-                String[] lines;
-                lines = line.split(" ");
-
-                if (rsl == 0 && (Objects.equals(lines[0], "400 ") || Objects.equals(lines[0], "500 "))) {
-                    out.println(lines[1] + ";");
+                String[] lines = line.split(" ");
+                if (rsl == 0 && (Objects.equals(lines[0], "400") || Objects.equals(lines[0], "500"))) {
+                    out.print(lines[1] + ";");
                     rsl = 1;
                 }
-                if (rsl == 1 && (Objects.equals(lines[0], "200 ") || Objects.equals(lines[0], "300 "))) {
-                    out.println(lines[1]);
-                    out.println(System.lineSeparator());
+                if (rsl == 1 && (Objects.equals(lines[0], "200") || Objects.equals(lines[0], "300"))) {
+                    out.print(lines[1] + ";");
+                    out.print(System.lineSeparator());
                     rsl = 0;
                 }
-
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     public static void main(String[] args) {
