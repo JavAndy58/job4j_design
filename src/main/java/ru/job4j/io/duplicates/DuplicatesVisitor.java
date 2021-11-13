d553ae0524d70f1e9771a881f0fe6290f79796da
JavAndy58 <digital.designs.russia@gmail.com>
2021-11-13 10:51:51 +0300

    8. Реализовать собственную структуру данных - HashMap

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,25 +5,23 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 import static java.nio.file.Files.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private final Map<Path, FileProperty> tempMap = new HashMap<>();
-
+    private final Set<FileProperty> duplicateSet = new HashSet<>();
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (isRegularFile(file)) {
             FileProperty fileProperty = new FileProperty(size(file), file.getFileName().toString());
-            if (tempMap.containsValue(fileProperty)) {
+            if (duplicateSet.contains(fileProperty)) {
                 System.out.println(file.toAbsolutePath());
             } else {
-                tempMap.put(file, fileProperty);
+                duplicateSet.add(fileProperty);
             }
         }
         return super.visitFile(file, attrs);
     }
-
 }
