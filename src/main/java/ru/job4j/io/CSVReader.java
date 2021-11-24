02a2e6e1c16320b6b7f52756c8cf8266366260b2
JavAndy58 <digital.designs.russia@gmail.com>
2021-11-24 18:29:28 +0300

    7. Scanner [#504791 #231577]-2

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -6,60 +6,37 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 public class CSVReader {
-    private static List<Integer> index = new ArrayList<>();
+
     public static void handle(ArgsName argsName) throws Exception {
 
         String argumentsLine = argsName.get("filter");
         String[] argumentsFilter = argumentsLine.split(",");
+        int[] switcher = new int[argumentsFilter.length];
 
         try (Scanner scanner = new Scanner(new FileReader(argsName.get("path")))) {
 
             while (scanner.hasNextLine()) {
-                String lineTemp;
-                String line;
-                String[] linesTemp;
-                lineTemp = scanner.nextLine();
-                line = lineTemp.substring(1, lineTemp.length() - 1);
-                linesTemp = line.split(";");
-
-                int[] switcher = new int[argumentsFilter.length];
+                String str;
+                String lineTemp = scanner.nextLine();
+                str = lineTemp.substring(1, lineTemp.length() - 1);
+                String[] linesTemp = str.split(";");
 
                 for (int i = 0; i < linesTemp.length; i++) {
                     if (Arrays.asList(argumentsFilter).contains(linesTemp[i])) {
                         switcher[i] = i;
                     }
-
                 }
-                System.out.println(Arrays.toString(switcher));
-//                for (int i = 0; i < linesTemp.length; i++) {
-//                    if (Arrays.binarySearch(switcher, i) >= 0) {
-//                        System.out.print(linesTemp[i]);
-//                    }
-//                }
-
+                for (int i = 0; i < linesTemp.length; i++) {
+                    if (Arrays.binarySearch(switcher, i) >= 0) {
+                        System.out.print(linesTemp[i] + " ");
+                    }
+                }
                 System.out.println();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-//    private static void searchIndex(ArgsName argsName) {
-//        String[] indexSearch;
-//        String argumentsLine = argsName.get("filter");
-//        String[] argumentsFilter = argumentsLine.split(",");
-//
-//        try (Scanner scanner = new Scanner(new FileReader(argsName.get("path")))) {
-//            System.out.println(Arrays.toString(scanner.nextLine().split(";")));
-//            indexSearch = scanner.nextLine().split(";");
-//            for (int i = 0; i < indexSearch.length; i++) {
-//               if (argumentsFilter.equals())
-//            }
-//            index.add(1);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
     public static void main(String[] args) throws Exception {
         handle(ArgsName.of(args));
