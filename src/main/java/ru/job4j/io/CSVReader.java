46e3b74ebd110866966e8904a0c2c6bf9240c37e
JavAndy58 <digital.designs.russia@gmail.com>
2021-11-25 14:57:53 +0300

    7. Scanner [#504791 #231577]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,9 +1,9 @@
 package ru.job4j.io;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
-import java.util.regex.Pattern;
 
 public class CSVReader {
 
@@ -18,9 +18,8 @@ public class CSVReader {
             while (scanner.hasNextLine()) {
                 String str;
                 String lineTemp = scanner.nextLine();
-                str = lineTemp.substring(1, lineTemp.length() - 1);
-                String[] linesTemp = str.split(";");
-
+                str = lineTemp.substring(0, lineTemp.length() - 1);
+                String[] linesTemp = str.split(argsName.get("delimiter"));
                 for (int i = 0; i < linesTemp.length; i++) {
                     if (Arrays.asList(argumentsFilter).contains(linesTemp[i])) {
                         switcher[i] = i;
@@ -28,10 +27,23 @@ public class CSVReader {
                 }
                 for (int i = 0; i < linesTemp.length; i++) {
                     if (Arrays.binarySearch(switcher, i) >= 0) {
-                        System.out.print(linesTemp[i] + " ");
+                        if (argsName.get("out").equals("stdout")) {
+                            if (i != switcher[switcher.length - 1]) {
+                                System.out.print(linesTemp[i] + ";");
+                            } else {
+                                System.out.println(linesTemp[i]);
+                            }
+                        } else {
+                            try (FileWriter writer = new FileWriter((argsName.get("out")), true)) {
+                                if (i != switcher[switcher.length - 1]) {
+                                    writer.write(linesTemp[i] + ";");
+                                } else {
+                                    writer.write(linesTemp[i] + System.lineSeparator());
+                                }
+                            }
+                        }
                     }
                 }
-                System.out.println();
             }
         } catch (Exception e) {
             e.printStackTrace();
