96f8e89dbad6e34381d016d97ab0e68603a8e3bf
JavAndy58 <digital.designs.russia@gmail.com>
2021-11-26 17:53:33 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -5,19 +5,26 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                         out.write("Pika HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        System.out.println(str);
-                        if (str.contains("Bye")) {
+                         out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    String str = in.readLine();
+                        if (str.contains("Exit")) {
                             server.close();
                         }
-                    }
+                        if (str.contains("Hello")) {
+                            out.write("Hello".getBytes());
+
+                        }
+                        if (!str.contains("Hello") && (!str.contains("Exit")) && str.length() != 0) {
+                            out.write("What".getBytes());
+
+                        }
                     out.flush();
                 }
             }
