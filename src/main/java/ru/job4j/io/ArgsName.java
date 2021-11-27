0a4752727973db7a208c4562eb3b8db0370c3a2d
JavAndy58 <digital.designs.russia@gmail.com>
2021-11-27 15:28:38 +0300

    7. Scanner [#504791 #231577]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -1,23 +1,15 @@
 package ru.job4j.io;
 
-import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ArgsName {
 
     private static Map<String, String> values = new HashMap<>();
-    private static final Pattern TEMPLATE_FILE = Pattern.compile("[a-zA-Z0-9\\.a-z]");
-    private static final Pattern TEMPLATE_DELIMITER = Pattern.compile("\\W");
-    private static final Pattern TEMPLATE_OUT = Pattern.compile("\\w");
-    private static final Pattern TEMPLATE_FILTER = Pattern.compile("([a-z],)+");
 
     public static ArgsName of(String[] args) {
         ArgsName names = new ArgsName();
         names.parse(args);
-        names.validation();
         return names;
     }
 
@@ -26,34 +18,10 @@ public class ArgsName {
     }
 
     private void parse(String[] args) throws IllegalArgumentException {
-        if (args.length != 4) {
-            throw new IllegalArgumentException("Введены не все необходимые данные для работы программы");
-        }
         String[] arguments;
         for (String argument : args) {
             arguments = argument.split("=");
             values.put(arguments[0].substring(1), arguments[1]);
-        }
-    }
-
-    private void validation() throws IllegalArgumentException {
-        Matcher matcherFile = TEMPLATE_FILE.matcher(get("path"));
-        File file = new File(get("path"));
-        if (!file.exists() && !file.isDirectory() && !matcherFile.find()) {
-            throw new IllegalArgumentException("Файл указан не правильно или не существует");
-        }
-        Matcher matcherDelimited = TEMPLATE_DELIMITER.matcher(get("delimiter"));
-        if (!matcherDelimited.find()) {
-            throw new IllegalArgumentException("Делитель указан не правильно");
-        }
-        Matcher matcherOut = TEMPLATE_OUT.matcher(get("out"));
-        File fileOut = new File(get("out"));
-        if (!matcherOut.find() && (!fileOut.exists() || get("out").equals("stdout"))) {
-            throw new IllegalArgumentException("Файл для вывода или stdout указаны не правильно");
-        }
-        Matcher matcherFilter = TEMPLATE_FILTER.matcher(get("filter"));
-        if (!matcherFilter.find()) {
-            throw new IllegalArgumentException("Фильтр указан не правильно");
         }
     }
 }
--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,14 +1,21 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.*;
 
 public class CSVReader {
+    private static final Pattern TEMPLATE_FILE = Pattern.compile("[a-zA-Z0-9\\.a-z]");
+    private static final Pattern TEMPLATE_DELIMITER = Pattern.compile("\\W");
+    private static final Pattern TEMPLATE_OUT = Pattern.compile("\\w");
+    private static final Pattern TEMPLATE_FILTER = Pattern.compile("([a-z],)+");
 
     public static void handle(ArgsName argsName) throws Exception {
 
+        validation(argsName);
         String argumentsLine = argsName.get("filter");
         String[] argumentsFilter = argumentsLine.split(",");
         int[] switcher = new int[argumentsFilter.length];
@@ -47,6 +54,27 @@ public class CSVReader {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void validation(ArgsName argsName) throws IllegalArgumentException {
+        Matcher matcherFile = TEMPLATE_FILE.matcher(argsName.get("path"));
+        File file = new File(argsName.get("path"));
+        if (!file.exists() && !file.isDirectory() && !matcherFile.find()) {
+            throw new IllegalArgumentException("Файл указан не правильно или не существует");
+        }
+        Matcher matcherDelimited = TEMPLATE_DELIMITER.matcher(argsName.get("delimiter"));
+        if (!matcherDelimited.find()) {
+            throw new IllegalArgumentException("Делитель указан не правильно");
+        }
+        Matcher matcherOut = TEMPLATE_OUT.matcher(argsName.get("out"));
+        File fileOut = new File(argsName.get("out"));
+        if (!matcherOut.find() && (!fileOut.exists() || argsName.get("out").equals("stdout"))) {
+            throw new IllegalArgumentException("Файл для вывода или stdout указаны не правильно");
+        }
+        Matcher matcherFilter = TEMPLATE_FILTER.matcher(argsName.get("filter"));
+        if (!matcherFilter.find()) {
+            throw new IllegalArgumentException("Фильтр указан не правильно");
         }
     }
 
