5f751b45928b0934d555c1298ec9bae5ba8dda93
JavAndy58 <digital.designs.russia@gmail.com>
2021-12-13 13:23:36 +0300

    1. Схема прав пользователи и роли. [#1093]

---
+++ data/insert.sql
@@ -0,0 +1,10 @@
+insert into role(name) values ('Роль');
+
+insert into users(name) values ('Egor');
+
+insert into category(name) values ('Срочные');
+
+insert into state(name) values ('В пути');
+
+insert into item(name, users_id, category_id, state_id) values ('Pen', 1, 1, 1);
+
