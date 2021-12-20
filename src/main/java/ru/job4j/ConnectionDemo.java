20772ffdf6e704865aae901311e60dd25d01ac36
JavAndy58 <digital.designs.russia@gmail.com>
2021-12-20 21:28:21 +0300

    0. JDBC [#6863]

--- src/main/java/ru/job4j/ConnectionDemo.java
+++ src/main/java/ru/job4j/ConnectionDemo.java
@@ -2,15 +2,17 @@ package ru.job4j;
 
 import ru.job4j.io.Config;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDemo {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         Class.forName("org.postgresql.Driver");
         Config config = new Config("app.properties");
+        config.load();
         String url = config.value("url");
         String login = config.value("login");
         String password = config.value("password");
